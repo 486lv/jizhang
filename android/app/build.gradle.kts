5217a2c0ba06cb08d661a08059f390d051400759
486lv <2876587146@qq.com>
2026-02-16 15:57:20 +0800

    new2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.laoba_jizhang"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21  // 需要至少API 21来支持权限处理
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
